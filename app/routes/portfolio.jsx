b406d6c90e1843af2384ee7546b0512c03f8a8b4
Andy Birosak <andyb@Andys-MacBook-Pro.local>
2022-03-02 15:22:32 -0500

    portfolio links added

--- app/routes/portfolio.jsx
+++ app/routes/portfolio.jsx
@@ -1,8 +1,39 @@
-
-
 function Portfolio() {
   return (
-    <div>Portfolio</div>
+    <div className='container'>
+      <h1>Portfolio</h1>
+      <div className='container'>
+        <a href='https://thehollowbk.org' rel='noreferrer' target='_blank'>
+          <h2>The Hollow</h2>
+        </a>
+        <p>
+          An ecommerce site for a Montessori-inspired early learning center.
+          Built with Shopify and custom Liquid elements.
+        </p>
+      </div>
+      <div className='container'>
+        <a href='https://birdie-lou.com' rel='noreferrer' target='blank'>
+          <h2>Birdie Lou</h2>
+        </a>
+        <p>
+          An ecommerce site for a childrens' wear. Built with Shopify and custom
+          Liquid elements.
+        </p>
+      </div>
+      <div className='container'>
+        <a
+          href='https://andyshopapp.herokuapp.com'
+          rel='noreferrer'
+          target='blank'
+        >
+          <h2>AndyShop</h2>
+        </a>
+        <p>
+          An ecommerce site designed to sell my own personal items. Built with
+          React and Redux.
+        </p>
+      </div>
+    </div>
   )
 }
 
